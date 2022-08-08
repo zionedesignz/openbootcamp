a4faac0fc724690a6f4577ee3bc1214c77fb9910
Ricardo López Álvarez <zionlsr@gmail.com>
2022-08-08 17:12:50 +0200

    Ejercicio 2

---
+++ Python/tema2/Ejercicio2.py
@@ -0,0 +1,5 @@
+texto = "Esto es una cadena de texto"
+print(texto)
+print("La variable texto es de tipo: ",type(texto))
+texto = "Esto es la cadena de texto que sobreescribe la variable texto"
+print(texto)
