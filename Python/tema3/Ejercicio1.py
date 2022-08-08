07443f685f0c6b1a0513d3982b3c73b157165826
Ricardo López Álvarez <zionlsr@gmail.com>
2022-08-08 19:00:54 +0200

    Ejercicio 1

---
+++ Python/tema3/Ejercicio1.py
@@ -0,0 +1,19 @@
+nombre = 'John'
+apellidos = 'Doe Garcia'
+edad = 35
+email = 'test@mail.com'
+telefono = 666555444
+casado = False
+tieneHijos = True
+listaDeAmigos = ['Joe','Harry','Bill']
+películasVistas = {'peli1': 'Harry el sucio','peli2': 'Terminator II'}
+
+print('Nombre: ',nombre)
+print('Apellidos: ',apellidos)
+print('Edad: ',edad)
+print('Email: ',email)
+print('Telefono: ',telefono)
+print('Casado: ',casado)
+print('Tiene hijos: ',tieneHijos)
+print('Amigos: ',listaDeAmigos)
+print('Peliculas vistas: ',películasVistas)
